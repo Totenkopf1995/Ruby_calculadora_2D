474d1c02befa87d1395c02473dcc9560eaeaa754
totenkopf1995 <uroxacin1@hotmail.com>
2024-01-13 01:29:46 -0500

    inicio calculadora

--- calculadora.rb
+++ calculadora.rb
@@ -1,2 +1,19 @@
 require 'gosu'
-require 'redis'
+
+
+class Calculadora < Gosu::Window
+  def initialize
+    super 400, 480
+    self.caption = "CALCULADORA"
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+end
+
+Calculadora.new.show
