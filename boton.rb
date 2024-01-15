eb0a58bc9938d1a019fd64cf410ae8c9b9d898ff
totenkopf1995 <uroxacin1@hotmail.com>
2024-01-14 20:52:33 -0500

    iniciando con la ventana

--- boton.rb
+++ boton.rb
@@ -2,8 +2,8 @@ require 'gosu'
 
 class Boton < Gosu::Window
   def initialize
-    super 200, 200
-    self.caption "hola mundo"
+    super 300, 400
+    self.caption = "hola mundo"
   end
   
   def update
